48800353ba1ef33a23102f217af7acfff8bc548d
rtarasov <rtarasov@gmail.com>
2017-09-23 22:35:10 +0300

    Add week_2 assignment

--- Angular/conFusion/app/scripts/app.js
+++ Angular/conFusion/app/scripts/app.js
@@ -75,7 +75,7 @@ angular.module('confusionApp',[]).controller('MenuController', ['$scope', functi
                     console.log('incorrect');
                 } else {
                     $scope.invalidChannelSelection = false;
-                    //the code like in exercise
+                    //the code like indicated in exercise
                     //$scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
                     $scope.feedback.mychannel = "";
 
@@ -93,4 +93,77 @@ angular.module('confusionApp',[]).controller('MenuController', ['$scope', functi
                     console.log($scope.feedback);
                 }
             };
+}]).controller('DishDetailController', ['$scope', function($scope) {
+            var dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains',
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+
+                           ]
+                    };
+
+            $scope.dish = dish;
+
+}]).controller('DishCommentController', ['$scope', function($scope) {
+
+            $scope.submitComment = function () {
+
+                $scope.comment.date = new Date().toISOString();
+
+                //Not number but string representation of rating added
+                //after form submit. This conversion is needed to sort correctly by the rating.
+                $scope.comment.rating = parseInt($scope.comment.rating, 10);
+
+                $scope.dish.comments.push($scope.comment);
+
+                $scope.commentForm.$setPristine();
+
+                $scope.comment = $scope.initComment();
+            };
+
+            $scope.initComment = function () {
+              return {
+                  rating:5,
+                  comment:"",
+                  author:"",
+                  date:""
+              };
+            };
+
+            $scope.comment = $scope.initComment();
+
 }]);
