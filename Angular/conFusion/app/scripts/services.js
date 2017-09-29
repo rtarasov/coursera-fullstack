5f92f0d4a88147e441945e24695db9cdb03efb45
rtarasov <rtarasov@gmail.com>
2017-09-29 20:52:51 +0300

    Add week_2 exercise_1 part 2

--- Angular/conFusion/app/scripts/services.js
+++ Angular/conFusion/app/scripts/services.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('confusionApp').factory('menuFactory', function() {
+angular.module('confusionApp').service('menuFactory', function() {
   var dishes=[
                {
                 name:'Uthapizza',
@@ -161,15 +161,12 @@ angular.module('confusionApp').factory('menuFactory', function() {
               }
   ];
 
-  var menufac = {};
-
-  menufac.getDishes = function(){
+  this.getDishes = function(){
     return dishes;
   };
 
-  menufac.getDish = function (index) {
+  this.getDish = function (index) {
     return dishes[index];
   };
 
-  return menufac;
 });
