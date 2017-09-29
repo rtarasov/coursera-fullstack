57eb02db6db1016f86ae959bba2be1acb8653f09
rtarasov <rtarasov@gmail.com>
2017-09-29 21:41:57 +0300

    Add week_3 exercise_2

--- Angular/conFusion/app/scripts/controllers.js
+++ Angular/conFusion/app/scripts/controllers.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('confusionApp').controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
     $scope.dishes = menuFactory.getDishes();
